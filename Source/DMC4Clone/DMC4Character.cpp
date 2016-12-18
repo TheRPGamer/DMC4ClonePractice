33fd417eda89ba536da6eaab1ee261b70520861e
Lihao Huang <huang_lihao@me.com>
2016-12-18 15:20:51 -0800

    Added blank DMC4Character Class. Base class for all playable characters

---
+++ Source/DMC4Clone/DMC4Character.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DMC4Clone.h"
+#include "DMC4Character.h"
+
+
+// Sets default values
+ADMC4Character::ADMC4Character()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADMC4Character::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADMC4Character::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+// Called to bind functionality to input
+void ADMC4Character::SetupPlayerInputComponent(class UInputComponent* InputComponent)
+{
+	Super::SetupPlayerInputComponent(InputComponent);
+
+}
+
